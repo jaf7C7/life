f1ded49796c3f196dd474429b0f64632e59d6ade
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-01 09:23:31 +0000

    Add feature: Dead grid cells can be toggled to life

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -6,3 +6,11 @@ test('should create a grid of cells', () => {
 
     expect(grid.cells).toEqual([]);
 });
+
+test('can toggle a dead cell to alive', () => {
+    const grid = createGrid();
+
+    grid.toggleCell(5, 5);
+
+    expect(grid.cells).toContainEqual([5, 5]);
+});
--- ui.js
+++ ui.js
@@ -1,5 +1,8 @@
 export function createGrid() {
     return {
         cells: [],
+        toggleCell(x, y) {
+            this.cells.push([x, y]);
+        },
     };
 }
