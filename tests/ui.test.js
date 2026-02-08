7992e32eb12e2949e755b8c6319fd818d49cc520
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-02-08 13:20:48 +0000

    Add feature: UI renders the grid

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -5,3 +5,9 @@ test('displays project title', async ({ page }) => {
 
     await expect(page).toHaveTitle(/Life/);
 });
+
+test('displays a grid of cells', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page.getByTestId('grid')).toBeVisible();
+});
