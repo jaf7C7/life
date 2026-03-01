02c2d5a34503d1b0391a3911f3022a201b9750f3
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-01 05:57:25 +0000

    Add feature: Grid centre always aligns with centre of a cell

--- life/ui.js
+++ life/ui.js
@@ -15,12 +15,7 @@ export default class UI {
         grid.setAttribute('data-testid', 'grid');
         document.body.appendChild(grid);
 
-        grid.height = 2000;
-        grid.width = 2000;
-        grid.cellSize = 20;
-        const ctx = grid.getContext('2d');
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, grid.width, grid.height);
+        this.drawGrid(grid);
 
         grid.addEventListener('click', () => {
             const ctx = grid.getContext('2d');
@@ -30,6 +25,43 @@ export default class UI {
             ctx.fillStyle = currentColor === red ? white : red;
             ctx.fillRect(0, 0, grid.width, grid.height);
         });
+
+        const resizeObserver = new ResizeObserver((entries) => {
+            this.drawGrid(entries[0].target);
+        });
+
+        resizeObserver.observe(grid);
+    }
+
+    drawGrid(grid) {
+        grid.cellSize = 20;
+        grid.width = grid.clientWidth;
+        grid.height = grid.clientHeight;
+        const ctx = grid.getContext('2d');
+
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, grid.width, grid.height);
+
+        const lineWidth = 2;
+        const effectiveCellSize = grid.cellSize + lineWidth;
+        ctx.fillStyle = 'white';
+
+        // Offset the grid so the centre of the middle cell always aligns
+        // with the centre of the grid.  The offset is such that a margin
+        // of 1 cell is drawn outside the boundary of the grid to avoid
+        // any blank space showing when resizing the grid element.
+        const offsetX =
+            ((grid.width / 2) % effectiveCellSize) -
+            effectiveCellSize * (3 / 2);
+        const offsetY =
+            ((grid.height / 2) % effectiveCellSize) -
+            effectiveCellSize * (3 / 2);
+
+        for (let y = offsetY; y < grid.height; y += effectiveCellSize) {
+            for (let x = offsetX; x < grid.width; x += effectiveCellSize) {
+                ctx.fillRect(x, y, grid.cellSize, grid.cellSize);
+            }
+        }
     }
 
     createElement() {}
