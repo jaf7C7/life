16174ea640551312946b44dcc130d49d945e0681
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-15 12:57:19 +0000

    Add dev dependencies: eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+const config = {
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'none',
+    overrides: [
+        {
+            files: ['*.html', '*.json'],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
+
+export default config;
