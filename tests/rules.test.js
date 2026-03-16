a6673420a522e34355163b33a1a8342f8c7bf08d
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-16 21:53:53 +0000

    Refactoring: Improve clarity of tests

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -3,14 +3,18 @@ import { test } from 'mocha';
 import { next } from '../life.js';
 
 test('A lone cell with no neighbours dies', () => {
-    const cells = [[0, 0]];
+    const loneCell = [0, 0];
+    const cells = [loneCell];
+
     assert.deepEqual([], next(cells));
 });
 
-test('A lone cell with one neighbour also dies', () => {
-    const cells = [
-        [0, 0],
-        [0, 1]
-    ];
+test('A cell with one neighbour also dies', () => {
+    const loneCell = [0, 0];
+    const neighbour = [1, 1];
+
+    // Both cells have only one neighbour and will both die.
+    const cells = [loneCell, neighbour];
+
     assert.deepEqual([], next(cells));
 });
