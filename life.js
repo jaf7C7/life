42ee2b7c1c6e3de1144e5426ad80766edb5edf5d
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-16 08:16:59 +0000

    Add feature: A lone cell with no neighbours dies

---
+++ life.js
@@ -0,0 +1,5 @@
+export function next(cells) {
+    if (cells.length === 1) {
+        return [];
+    }
+}
--- tests/rules.test.js
+++ tests/rules.test.js
@@ -1,6 +1,8 @@
 import assert from 'node:assert';
 import { test } from 'mocha';
+import { next } from '../life.js';
 
-test('Everything is imported correctly', () => {
-    assert(true);
+test('A lone cell with no neighbours dies', () => {
+    const cells = [[0, 0]];
+    assert.deepEqual([], next(cells));
 });
