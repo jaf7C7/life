291190459f3a78c5bd68c276a218f58c9e52037f
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-17 06:58:25 +0000

    Add markdown formatting rule

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,7 @@ const config = {
     trailingComma: 'none',
     overrides: [
         {
-            files: ['*.html', '*.json'],
+            files: ['*.html', '*.json', '*.md'],
             options: {
                 tabWidth: 2
             }
