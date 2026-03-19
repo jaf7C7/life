64bedd09cbe338499cff8dcfba847087a6d18b73
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-19 10:14:48 +0000

    Refactoring: Extract functions to global scope

--- life/rules.js
+++ life/rules.js
@@ -16,6 +16,27 @@ function contains(cell, cells) {
     return cells.find(([x, y]) => x === cell[0] && y === cell[1]) !== undefined;
 }
 
+function equals(cell, otherCell) {
+    return cell[0] === otherCell[0] && cell[1] === otherCell[1];
+}
+
+function count(cell, cells) {
+    return cells.reduce((counter, c) => counter + (equals(cell, c) ? 1 : 0), 0);
+}
+
+function resurrectees(deadNeighbours) {
+    const resurrectees = [];
+    for (const cell of deadNeighbours) {
+        if (
+            count(cell, deadNeighbours) === 3 &&
+            !contains(cell, resurrectees)
+        ) {
+            resurrectees.push(cell);
+        }
+    }
+    return resurrectees;
+}
+
 export function next(cells) {
     let newCells = [];
     const deadNeighbours = [];
@@ -34,30 +55,6 @@ export function next(cells) {
         if (liveNeighbours.length > 1) {
             newCells.push(cell);
         }
-    }
-
-    function equals(cell, otherCell) {
-        return cell[0] === otherCell[0] && cell[1] === otherCell[1];
-    }
-
-    function count(cell, cells) {
-        return cells.reduce(
-            (counter, c) => counter + (equals(cell, c) ? 1 : 0),
-            0
-        );
-    }
-
-    function resurrectees(deadNeighbours) {
-        const resurrectees = [];
-        for (const cell of deadNeighbours) {
-            if (
-                count(cell, deadNeighbours) === 3 &&
-                !contains(cell, resurrectees)
-            ) {
-                resurrectees.push(cell);
-            }
-        }
-        return resurrectees;
     }
 
     newCells = resurrectees(deadNeighbours).concat(newCells);
