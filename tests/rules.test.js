aea9a2592dff7c2b3e6fd04d76ed80457b701089
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-20 09:22:27 +0000

    Make order of cells unimportant in test assertions

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -3,23 +3,27 @@ import { test } from 'mocha';
 import { next } from '../life/rules.js';
 
 test('A cell with no neighbours dies', () => {
+    const cells = new Set();
     const cell = [0, 0];
-    const cells = [cell];
+    cells.add(cell);
 
-    assert.deepEqual(next(cells), []);
+    assert.deepEqual(next(cells), new Set());
 });
 
 test('A cell with one neighbour also dies', () => {
+    const cells = new Set();
     const cell = [0, 0];
     const neighbour = [1, 1];
 
     // Both cells have only one neighbour and will both die.
-    const cells = [cell, neighbour];
+    cells.add(cell);
+    cells.add(neighbour);
 
-    assert.deepEqual(next(cells), []);
+    assert.deepEqual(next(cells), new Set());
 });
 
 test('A cell with two neighbours survives', () => {
+    const cells = new Set();
     const cell = [0, 0];
     const neighbours = [
         [-1, -1],
@@ -28,12 +32,16 @@ test('A cell with two neighbours survives', () => {
 
     // Cells `[-1, -1]` and `[1, 1]` each have less than two neighbours and so
     // will not survive.
-    const cells = [cell, ...neighbours];
+    cells.add(cell);
+    for (const n of neighbours) {
+        cells.add(n);
+    }
 
-    assert.deepEqual(next(cells), [cell]);
+    assert.deepEqual(next(cells), new Set([cell]));
 });
 
 test('A dead cell with exactly three live neighbours becomes alive', () => {
+    const cells = new Set();
     const deadCell = [0, 0];
     const liveNeighbours = [
         [-1, 0],
@@ -43,7 +51,9 @@ test('A dead cell with exactly three live neighbours becomes alive', () => {
 
     // The neighbour cells each have two neighbours, and so survive. The dead
     // cell [0, 0] has three neighbours and so comes to life.
-    const cells = [...liveNeighbours];
+    for (const n of liveNeighbours) {
+        cells.add(n);
+    }
 
-    assert.deepEqual(next(cells), [deadCell, ...cells]);
+    assert.deepEqual(next(cells), new Set([deadCell, ...cells]));
 });
