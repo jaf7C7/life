8f5db33805d63e45146a752cb84cd651e35c9e58
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-20 18:42:57 +0000

    Refactoring: Represent cells as strings everywhere

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -3,15 +3,15 @@ import { test } from 'mocha';
 import { next } from '../life/rules.js';
 
 test('A cell with no neighbours dies', () => {
-    const cell = [0, 0];
+    const cell = '0,0';
     const cells = new Set([cell]);
 
     assert.deepEqual(next(cells), new Set());
 });
 
 test('A cell with one neighbour also dies', () => {
-    const cell = [0, 0];
-    const neighbour = [1, 1];
+    const cell = '0,0';
+    const neighbour = '1,1';
 
     // Both cells have only one neighbour and will both die.
     const cells = new Set([cell, neighbour]);
@@ -20,13 +20,10 @@ test('A cell with one neighbour also dies', () => {
 });
 
 test('A cell with two neighbours survives', () => {
-    const cell = [0, 0];
-    const neighbours = [
-        [-1, -1],
-        [1, 1]
-    ];
+    const cell = '0,0';
+    const neighbours = ['-1,-1', '1,1'];
 
-    // Cells `[-1, -1]` and `[1, 1]` each have less than two neighbours and so
+    // Cells `'-1,-1'` and `'1,1'` each have less than two neighbours and so
     // will not survive.
     const cells = new Set([cell, ...neighbours]);
 
@@ -34,15 +31,11 @@ test('A cell with two neighbours survives', () => {
 });
 
 test('A dead cell with exactly three live neighbours becomes alive', () => {
-    const deadCell = [0, 0];
-    const liveNeighbours = [
-        [-1, 0],
-        [-1, -1],
-        [0, -1]
-    ];
+    const deadCell = '0,0';
+    const liveNeighbours = ['-1,0', '-1,-1', '0,-1'];
 
     // The neighbour cells each have two neighbours, and so survive. The dead
-    // cell [0, 0] has three neighbours and so comes to life.
+    // cell '0,0' has three neighbours and so comes to life.
     const cells = new Set(liveNeighbours);
 
     assert.deepEqual(next(cells), new Set([deadCell, ...cells]));
