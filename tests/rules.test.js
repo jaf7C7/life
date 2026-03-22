380eb5979851784034aa68d0bfb99723274c95a8
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-22 11:28:43 +0000

    Refactoring: Remove unnecessary comments from tests

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -12,8 +12,6 @@ test('A cell with no neighbours dies', () => {
 test('A cell with one neighbour also dies', () => {
     const cell = '0,0';
     const neighbour = '1,1';
-
-    // Both cells have only one neighbour and will both die.
     const cells = new Set([cell, neighbour]);
 
     expect(next(cells)).not.to.include(cell);
@@ -22,9 +20,6 @@ test('A cell with one neighbour also dies', () => {
 test('A cell with two neighbours survives', () => {
     const cell = '0,0';
     const neighbours = ['-1,-1', '1,1'];
-
-    // Cells `'-1,-1'` and `'1,1'` each have less than two neighbours and so
-    // will not survive.
     const cells = new Set([cell, ...neighbours]);
 
     expect(next(cells)).to.include(cell);
@@ -33,9 +28,6 @@ test('A cell with two neighbours survives', () => {
 test('A dead cell with exactly three live neighbours becomes alive', () => {
     const deadCell = '0,0';
     const liveNeighbours = ['-1,0', '-1,-1', '0,-1'];
-
-    // The neighbour cells each have two neighbours, and so survive. The dead
-    // cell '0,0' has three neighbours and so comes to life.
     const cells = new Set(liveNeighbours);
 
     expect(next(cells)).to.include(deadCell);
@@ -44,16 +36,6 @@ test('A dead cell with exactly three live neighbours becomes alive', () => {
 test('A live cell with more than three live neighbours dies', () => {
     const cell = '0,0';
     const neighbours = ['-1,-1', '-1,1', '1,1', '1,-1'];
-
-    // The neighbour cells each have one neighbour - '0,0', and so will die.
-    // The central cell has four neighbours and so will also die. The dead
-    // cells '-1,0', '0,1', '1,0', and '0,-1' all have three neighbours and so
-    // come to life.
-    //
-    //     x x     x
-    //      x  -> x x
-    //     x x     x
-    //
     const cells = new Set([cell, ...neighbours]);
 
     expect(next(cells)).not.to.include(cell);
@@ -62,9 +44,6 @@ test('A live cell with more than three live neighbours dies', () => {
 test('A live cell with three live neighbours also survives', () => {
     const cell = '0,0';
     const neighbours = ['-1,0', '-1,1', '0,1'];
-
-    // Each cell has exactly three neighbours so all cells survive and the the
-    // pattern is stable.
     const cells = new Set([cell, ...neighbours]);
 
     expect(next(cells)).to.include(cell);
