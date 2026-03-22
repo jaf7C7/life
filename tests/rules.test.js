f70601f3988c8e81c166f75833977851573b4dfb
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-22 19:55:26 +0000

    Make test output more readable

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -1,60 +1,63 @@
 import { expect } from 'chai';
-import { test } from 'mocha';
+import { describe, it } from 'mocha';
 import { next } from '../life/rules.js';
+describe('Basic rules', () => {
+    it('A cell with no neighbours dies', () => {
+        const cell = '0,0';
+        const cells = new Set([cell]);
 
-test('A cell with no neighbours dies', () => {
-    const cell = '0,0';
-    const cells = new Set([cell]);
+        expect(next(cells)).not.to.include(cell);
+    });
 
-    expect(next(cells)).not.to.include(cell);
+    it('A cell with one neighbour also dies', () => {
+        const cell = '0,0';
+        const neighbour = '1,1';
+        const cells = new Set([cell, neighbour]);
+
+        expect(next(cells)).not.to.include(cell);
+    });
+
+    it('A cell with two neighbours survives', () => {
+        const cell = '0,0';
+        const neighbours = ['-1,-1', '1,1'];
+        const cells = new Set([cell, ...neighbours]);
+
+        expect(next(cells)).to.include(cell);
+    });
+
+    it('A dead cell with exactly three live neighbours becomes alive', () => {
+        const deadCell = '0,0';
+        const liveNeighbours = ['-1,0', '-1,-1', '0,-1'];
+        const cells = new Set(liveNeighbours);
+
+        expect(next(cells)).to.include(deadCell);
+    });
+
+    it('A live cell with more than three live neighbours dies', () => {
+        const cell = '0,0';
+        const neighbours = ['-1,-1', '-1,1', '1,1', '1,-1'];
+        const cells = new Set([cell, ...neighbours]);
+
+        expect(next(cells)).not.to.include(cell);
+    });
+
+    it('A live cell with three live neighbours also survives', () => {
+        const cell = '0,0';
+        const neighbours = ['-1,0', '-1,1', '0,1'];
+        const cells = new Set([cell, ...neighbours]);
+
+        expect(next(cells)).to.include(cell);
+    });
 });
 
-test('A cell with one neighbour also dies', () => {
-    const cell = '0,0';
-    const neighbour = '1,1';
-    const cells = new Set([cell, neighbour]);
+describe('Blinker oscillator', () => {
+    it('Has a period of two', () => {
+        // The blinker the simplest oscillator in 'Life'. It oscillates between
+        // a 3-cell column and a 3-cell row, with a period of 2.
+        // See: https://conways-game-of-life.fandom.com/wiki/Blinker
+        const blinker = ['0,0', '0,1', '0,-1'];
+        const cells = new Set(blinker);
 
-    expect(next(cells)).not.to.include(cell);
-});
-
-test('A cell with two neighbours survives', () => {
-    const cell = '0,0';
-    const neighbours = ['-1,-1', '1,1'];
-    const cells = new Set([cell, ...neighbours]);
-
-    expect(next(cells)).to.include(cell);
-});
-
-test('A dead cell with exactly three live neighbours becomes alive', () => {
-    const deadCell = '0,0';
-    const liveNeighbours = ['-1,0', '-1,-1', '0,-1'];
-    const cells = new Set(liveNeighbours);
-
-    expect(next(cells)).to.include(deadCell);
-});
-
-test('A live cell with more than three live neighbours dies', () => {
-    const cell = '0,0';
-    const neighbours = ['-1,-1', '-1,1', '1,1', '1,-1'];
-    const cells = new Set([cell, ...neighbours]);
-
-    expect(next(cells)).not.to.include(cell);
-});
-
-test('A live cell with three live neighbours also survives', () => {
-    const cell = '0,0';
-    const neighbours = ['-1,0', '-1,1', '0,1'];
-    const cells = new Set([cell, ...neighbours]);
-
-    expect(next(cells)).to.include(cell);
-});
-
-test('The blinker oscillator', () => {
-    // The blinker the simplest oscillator in 'Life'. It oscillates between
-    // a 3-cell column and a 3-cell row, with a period of 2.
-    // See: https://conways-game-of-life.fandom.com/wiki/Blinker
-    const blinker = ['0,0', '0,1', '0,-1'];
-    const cells = new Set(blinker);
-
-    expect(next(next(cells))).to.deep.equal(cells);
+        expect(next(next(cells))).to.deep.equal(cells);
+    });
 });
