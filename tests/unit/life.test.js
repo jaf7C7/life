f2ca24b6b8e6fcc5307e17fab47f44db7913d075
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-25 07:37:07 +0000

    Add feature: Clicking the centre of the canvas toggles cell "0,0"

--- tests/unit/life.test.js
+++ tests/unit/life.test.js
@@ -17,4 +17,34 @@ suite('User Interface', () => {
 
         expect(elementCreated).to.equal('canvas');
     });
+
+    test('Clicking on the center of the canvas toggles cell "0,0"', () => {
+        const cells = new Set();
+        const elements = [];
+
+        function createElement(type) {
+            elements.push({
+                type,
+                width: 100,
+                height: 100,
+                click({ position: { x, y } }) {
+                    if (x === 50 && y === 50) {
+                        cells.add('0,0');
+                    }
+                }
+            });
+        }
+
+        function findElement(type) {
+            return elements.find((e) => e.type === type);
+        }
+
+        initApp(createElement, cells);
+        const canvas = findElement('canvas');
+        canvas.click({
+            position: { x: canvas.width / 2, y: canvas.height / 2 }
+        });
+
+        expect(cells).to.deep.equal(new Set(['0,0']));
+    });
 });
