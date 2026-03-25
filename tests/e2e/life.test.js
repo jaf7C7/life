635d52f530f0b2f564cbce1bb2890a4395d5f6e1
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-25 09:19:18 +0000

    Add feature: The canvas has nonzero size

--- tests/e2e/life.test.js
+++ tests/e2e/life.test.js
@@ -5,3 +5,16 @@ test('Has a canvas element', async ({ page }) => {
 
     await expect(page.getByTestId('canvas')).toBeVisible();
 });
+
+test('The canvas has nonzero size', async ({ page }) => {
+    await page.goto('/');
+    const canvas = await page.getByTestId('canvas');
+    const { width, height } = await canvas.boundingBox();
+
+    expect(
+        await page.evaluate(({ x, y }) => x > 0 && y > 0, {
+            x: width,
+            y: height
+        })
+    ).toBeTruthy();
+});
