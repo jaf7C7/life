8c58db6b6ef00217e2765c93415143ce54db87cf
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-25 08:21:04 +0000

    Refactoring: Replace hard-coded value with calculated one

--- tests/unit/life.test.js
+++ tests/unit/life.test.js
@@ -28,7 +28,7 @@ suite('User Interface', () => {
                 width: 100,
                 height: 100,
                 click({ position: { x, y } }) {
-                    if (x === 50 && y === 50) {
+                    if (x === this.width / 2 && y === this.height / 2) {
                         cells.add('0,0');
                     }
                 }
