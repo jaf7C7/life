95ce52af9f1d17667cee001f6db79d5001f9e3ed
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-26 07:53:53 +0000

    Add feature: The single cell at the centre of the canvas is white

--- tests/e2e/life.test.js
+++ tests/e2e/life.test.js
@@ -6,15 +6,27 @@ test('Has a canvas element', async ({ page }) => {
     await expect(page.getByTestId('canvas')).toBeVisible();
 });
 
-test('The canvas has nonzero size', async ({ page }) => {
+test('The single cell at the centre of the canvas is white', async ({
+    page
+}) => {
     await page.goto('/');
     const canvas = await page.getByTestId('canvas');
     const { width, height } = await canvas.boundingBox();
 
     expect(
-        await page.evaluate(({ x, y }) => x > 0 && y > 0, {
-            x: width,
-            y: height
-        })
+        await page.evaluate(
+            ({ x, y }) => {
+                const cellSize = 20;
+                const canvas = document.querySelector('canvas');
+                const ctx = canvas.getContext('2d');
+                const imgData = ctx.getImageData(x, y, cellSize, cellSize).data;
+
+                return imgData.every((e) => e === 255);
+            },
+            {
+                x: width / 2,
+                y: height / 2
+            }
+        )
     ).toBeTruthy();
 });
