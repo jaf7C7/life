3bdbf747eed9df740ef48e45a3db7a47ed03e9e7
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-28 06:00:47 +0000

    Refactoring: Extract helper function to top level

--- tests/e2e/life.test.js
+++ tests/e2e/life.test.js
@@ -6,6 +6,33 @@ test('Has a canvas element', async ({ page }) => {
     await expect(page.getByTestId('canvas')).toBeVisible();
 });
 
+function cellIsAlive({ x0, y0, effectiveCellSize }) {
+    function isBorderPixel(x, y, effectiveCellSize) {
+        const max = effectiveCellSize - 1;
+        return x === 0 || x === max || y === 0 || y === max;
+    }
+
+    function isBorderColor([r, g, b, a]) {
+        return r === 0 && g === 0 && b === 0 && a === 255; // black
+    }
+
+    function isCellColor([r, g, b, a]) {
+        return r === 255 && g === 255 && b === 255 && a === 255; // white
+    }
+
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+
+    for (let i = 0; i < effectiveCellSize; i++) {
+        for (let j = 0; j < effectiveCellSize; j++) {
+            const pixelData = ctx.getImageData(x0 + i, y0 + j, 1, 1).data;
+            return isBorderPixel(i, j, effectiveCellSize)
+                ? isBorderColor(pixelData)
+                : isCellColor(pixelData);
+        }
+    }
+}
+
 test('The central cell is white with a black border', async ({ page }) => {
     await page.goto('/');
     const canvas = await page.getByTestId('canvas');
@@ -19,39 +46,6 @@ test('The central cell is white with a black border', async ({ page }) => {
     const y0 = height / 2 - effectiveCellSize / 2;
 
     expect(
-        await page.evaluate(
-            ({ x0, y0, effectiveCellSize }) => {
-                function isBorderPixel(x, y, effectiveCellSize) {
-                    const max = effectiveCellSize - 1;
-                    return x === 0 || x === max || y === 0 || y === max;
-                }
-
-                function isBorderColor([r, g, b, a]) {
-                    return r === 0 && g === 0 && b === 0 && a === 255; // black
-                }
-
-                function isCellColor([r, g, b, a]) {
-                    return r === 255 && g === 255 && b === 255 && a === 255; // white
-                }
-
-                const canvas = document.querySelector('canvas');
-                const ctx = canvas.getContext('2d');
-
-                for (let i = 0; i < effectiveCellSize; i++) {
-                    for (let j = 0; j < effectiveCellSize; j++) {
-                        const pixelData = ctx.getImageData(
-                            x0 + i,
-                            y0 + j,
-                            1,
-                            1
-                        ).data;
-                        return isBorderPixel(i, j, effectiveCellSize)
-                            ? isBorderColor(pixelData)
-                            : isCellColor(pixelData);
-                    }
-                }
-            },
-            { x0, y0, effectiveCellSize }
-        )
+        await page.evaluate(cellIsAlive, { x0, y0, effectiveCellSize })
     ).toBeTruthy();
 });
