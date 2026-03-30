9b378b1be2aa0ac1b625b209378d8a51696ddf13
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-30 18:46:57 +0100

    Refactoring: Extract function

--- tests/e2e/life.test.js
+++ tests/e2e/life.test.js
@@ -7,6 +7,18 @@ test('Has a canvas element', async ({ page }) => {
     await expect(page.getByTestId('canvas')).toBeVisible();
 });
 
+function cellCoords(x, y, width, height, cellSize, borderWidth) {
+    // Coords of top left corner of cell `1,1`. Slightly confusing because the
+    // cell co-ords have their origin at the centre of the canvas, and y
+    // increases in the upwards direction, whereas the canvas co-ords have
+    // their origin at the top left corner of the canvas, and y increases in
+    // the downwards direction.
+    const x0 = width / 2 - (cellSize + borderWidth) / 2 + x * (cellSize + borderWidth);
+    const y0 = height / 2 - (cellSize + borderWidth) / 2 - y * (cellSize + borderWidth);
+
+    return [x0, y0];
+}
+
 test('The central cell is white with a black border', async ({ page }) => {
     await page.goto('/');
     const canvas = await page.getByTestId('canvas');
@@ -14,10 +26,7 @@ test('The central cell is white with a black border', async ({ page }) => {
     const cellSize = 20;
     const borderWidth = 2;
 
-    // `x0` and `y0` are the coords of the top-left corner of the central cell.
-    const [x0, y0] = [width, height].map(
-        (e) => (e - cellSize - borderWidth) / 2
-    );
+    const [x0, y0] = cellCoords(0, 0, width, height, cellSize, borderWidth);
 
     const result = await page.evaluate(cellIsRendered, {
         x0,
@@ -36,13 +45,7 @@ test('Cell `1,1` is white with a black border', async ({ page }) => {
     const cellSize = 20;
     const borderWidth = 2;
 
-    // Coords of top left corner of cell `1,1`. Slightly confusing because the
-    // cell co-ords have their origin at the centre of the canvas, and y
-    // increases in the upwards direction, whereas the canvas co-ords have
-    // their origin at the top left corner of the canvas, and y increases in
-    // the downwards direction.
-    const x0 = (width - cellSize - borderWidth) / 2 + (cellSize + borderWidth);
-    const y0 = (height - cellSize - borderWidth) / 2 - (cellSize + borderWidth);
+    const [x0, y0] = cellCoords(1, 1, width, height, cellSize, borderWidth);
 
     const result = await page.evaluate(cellIsRendered, {
         x0,
