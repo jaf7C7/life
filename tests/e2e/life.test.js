7c3a3007fd30c40eea6460dff247100623d29b50
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-03-30 17:43:36 +0100

    Add feature: Cell `1,1` is white with a black border

--- tests/e2e/life.test.js
+++ tests/e2e/life.test.js
@@ -28,3 +28,28 @@ test('The central cell is white with a black border', async ({ page }) => {
 
     expect(result).toBeTruthy();
 });
+
+test('Cell `1,1` is white with a black border', async ({ page }) => {
+    await page.goto('/');
+    const canvas = await page.getByTestId('canvas');
+    const { width, height } = await canvas.boundingBox();
+    const cellSize = 20;
+    const borderWidth = 2;
+
+    // Coords of top left corner of cell `1,1`. Slightly confusing because the
+    // cell co-ords have their origin at the centre of the canvas, and y
+    // increases in the upwards direction, whereas the canvas co-ords have
+    // their origin at the top left corner of the canvas, and y increases in
+    // the downwards direction.
+    const x0 = (width - cellSize - borderWidth) / 2 + (cellSize + borderWidth);
+    const y0 = (height - cellSize - borderWidth) / 2 - (cellSize + borderWidth);
+
+    const result = await page.evaluate(cellIsRendered, {
+        x0,
+        y0,
+        cellSize,
+        borderWidth
+    });
+
+    expect(result).toBeTruthy();
+});
