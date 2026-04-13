544ace76d04ca654c363c5aa878f1bd439875b22
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-04-13 21:20:38 +0100

    Refactoring: Decompose object into loose variables

--- life/app.js
+++ life/app.js
@@ -1,9 +1,9 @@
 import { Cell } from './cell.js';
 import { render } from './render.js';
 
-function cellFromScreen(offsetX, offsetY, origin) {
-    const cellX = Math.floor((offsetX - origin.x) / Cell.step);
-    const cellY = -Math.floor((offsetY - origin.y) / Cell.step);
+function cellFromScreen(offsetX, offsetY, originX, originY) {
+    const cellX = Math.floor((offsetX - originX) / Cell.step);
+    const cellY = -Math.floor((offsetY - originY) / Cell.step);
 
     return new Cell(cellX, cellY);
 }
@@ -16,23 +16,29 @@ function toggleCell(cell, cells) {
     }
 }
 
-function createClickHandler(ui, canvas, origin, cells) {
+function createClickHandler(ui, canvas, originX, originY, cells) {
     return ({ offsetX, offsetY }) => {
-        const cell = cellFromScreen(offsetX, offsetY, origin).toString();
+        const cell = cellFromScreen(
+            offsetX,
+            offsetY,
+            originX,
+            originY
+        ).toString();
         toggleCell(cell, cells);
+
+        const origin = { x: originX, y: originY };
         render(canvas, cells, origin);
     };
 }
 
 export function initApp(ui, cells) {
     const canvas = ui.createElement('canvas');
-    const origin = {
-        x: canvas.width / 2 - Cell.step / 2,
-        y: canvas.height / 2 - Cell.step / 2
-    };
+    const originX = canvas.width / 2 - Cell.step / 2;
+    const originY = canvas.height / 2 - Cell.step / 2;
 
-    const handleClick = createClickHandler(ui, canvas, origin, cells);
+    const handleClick = createClickHandler(ui, canvas, originX, originY, cells);
     canvas.addEventListener('click', handleClick);
 
+    const origin = { x: originX, y: originY };
     render(canvas, cells, origin);
 }
