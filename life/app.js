809c98f089196fb792c92b81097fd16a8fa2b890
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-04-13 21:31:24 +0100

    Refactoring: Recombine modules to re-consider abstractions

--- life/app.js
+++ life/app.js
@@ -1,5 +1,45 @@
 import { Cell } from './cell.js';
-import { render } from './render.js';
+
+function cellToScreenX(originX, x) {
+    return originX + x * Cell.step + Cell.borderWidth / 2;
+}
+
+function cellToScreenY(originY, y) {
+    return originY - y * Cell.step + Cell.borderWidth / 2;
+}
+
+function renderCell(ctx, originX, originY, cell, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(
+        cellToScreenX(originX, cell.x),
+        cellToScreenY(originY, cell.y),
+        Cell.size,
+        Cell.size
+    );
+}
+
+function render(canvas, cells, originX, originY) {
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = Cell.borderColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const minX = Math.floor(-originX / Cell.step);
+    const maxX = Math.floor((canvas.width - originX) / Cell.step);
+    const minY = Math.floor(-(canvas.height - originY) / Cell.step);
+    const maxY = Math.ceil(originY / Cell.step);
+
+    for (let x = minX; x <= maxX; x++) {
+        for (let y = minY; y <= maxY; y++) {
+            const cell = new Cell(x, y);
+            renderCell(ctx, originX, originY, cell, Cell.deadColor);
+        }
+    }
+
+    for (const cell of [...cells].map(Cell.fromString)) {
+        renderCell(ctx, originX, originY, cell, Cell.aliveColor);
+    }
+}
 
 function cellFromScreen(offsetX, offsetY, originX, originY) {
     const cellX = Math.floor((offsetX - originX) / Cell.step);
@@ -24,10 +64,10 @@ function createClickHandler(ui, canvas, originX, originY, cells) {
             originX,
             originY
         ).toString();
+
         toggleCell(cell, cells);
 
-        const origin = { x: originX, y: originY };
-        render(canvas, cells, origin);
+        render(canvas, cells, originX, originY);
     };
 }
 
@@ -39,6 +79,5 @@ export function initApp(ui, cells) {
     const handleClick = createClickHandler(ui, canvas, originX, originY, cells);
     canvas.addEventListener('click', handleClick);
 
-    const origin = { x: originX, y: originY };
-    render(canvas, cells, origin);
+    render(canvas, cells, originX, originY);
 }
