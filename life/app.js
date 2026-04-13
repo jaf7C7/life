5a3cf5fe8fa2c8d26e7e748e8896e3049e97f16b
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-04-13 12:52:33 +0100

    Refactoring: Un-destructure parameter to simplify code

--- life/app.js
+++ life/app.js
@@ -8,44 +8,45 @@ function cellToScreenY(originY, y) {
     return originY - y * Cell.step + Cell.borderWidth / 2;
 }
 
-function renderCell(ctx, { originX, originY }, cell, color) {
+function renderCell(ctx, origin, cell, color) {
     ctx.fillStyle = color;
     ctx.fillRect(
-        cellToScreenX(originX, cell.x),
-        cellToScreenY(originY, cell.y),
+        cellToScreenX(origin.x, cell.x),
+        cellToScreenY(origin.y, cell.y),
         Cell.size,
         Cell.size
     );
 }
 
-function render(canvas, cells, { originX, originY }) {
+function render(canvas, cells, origin) {
     const ctx = canvas.getContext('2d');
-    const origin = { originX, originY };
 
     ctx.fillStyle = Cell.borderColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    const minX = Math.floor(-originX / Cell.step);
-    const maxX = Math.floor((canvas.width - originX) / Cell.step);
-    const minY = Math.floor(-(canvas.height - originY) / Cell.step);
-    const maxY = Math.ceil(originY / Cell.step);
+    const minX = Math.floor(-origin.x / Cell.step);
+    const maxX = Math.floor((canvas.width - origin.x) / Cell.step);
+    const minY = Math.floor(-(canvas.height - origin.y) / Cell.step);
+    const maxY = Math.ceil(origin.y / Cell.step);
 
     for (let x = minX; x <= maxX; x++) {
         for (let y = minY; y <= maxY; y++) {
             const cell = new Cell(x, y);
-            renderCell(ctx, origin, cell, Cell.deadColor, originX, originY);
+            renderCell(ctx, origin, cell, Cell.deadColor, origin.x, origin.y);
         }
     }
 
     for (const cell of [...cells].map(Cell.fromString)) {
-        renderCell(ctx, origin, cell, Cell.aliveColor, originX, originY);
+        renderCell(ctx, origin, cell, Cell.aliveColor, origin.x, origin.y);
     }
 }
 
 export function initApp(ui, cells) {
     const canvas = ui.createElement('canvas');
-    const originX = canvas.width / 2 - Cell.step / 2;
-    const originY = canvas.height / 2 - Cell.step / 2;
+    const origin = {
+        x: canvas.width / 2 - Cell.step / 2,
+        y: canvas.height / 2 - Cell.step / 2
+    };
 
     let panX = 0,
         panY = 0;
@@ -67,10 +68,10 @@ export function initApp(ui, cells) {
 
     canvas.addEventListener('click', (event) => {
         const cellX = Math.floor(
-            (event.offsetX - (originX + panX)) / Cell.step
+            (event.offsetX - (origin.x + panX)) / Cell.step
         );
         const cellY = -Math.floor(
-            (event.offsetY - (originY + panY)) / Cell.step
+            (event.offsetY - (origin.y + panY)) / Cell.step
         );
         const cell = new Cell(cellX, cellY).toString();
         if (cells.has(cell)) {
@@ -78,8 +79,8 @@ export function initApp(ui, cells) {
         } else {
             cells.add(cell);
         }
-        render(canvas, cells, { originX, originY });
+        render(canvas, cells, origin);
     });
 
-    render(canvas, cells, { originX, originY });
+    render(canvas, cells, origin);
 }
