42c8eca759310369c592099a7a3cbabee5143bc2
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2026-09-21 18:21:11 +0100

    avoid vacuous assertion by asserting intermediate state

--- tests/e2e/life.test.js
+++ tests/e2e/life.test.js
@@ -34,7 +34,8 @@ test('Clicking on a cell twice leaves it dead', async ({ page }) => {
     const canvas = await RenderedCanvas.fromPage(page);
 
     await canvas.clickCell(1, 1);
-    await canvas.clickCell(1, 1);
+    expect((await canvas.cell(1, 1)).isAlive()).toBe(true);
 
-    expect((await canvas.cell(1, 1)).isAlive()).toBe(false);
+    await canvas.clickCell(1, 1);
+    expect((await canvas.cell(1, 1)).isDead()).toBe(true);
 });
